ec8899f4f0f7377ab2ff55ae378a776ce107b10d
Lenny Maiorani <ldm5180@gmail.com>
2016-12-15 02:34:24 +0000

    AoC 2016 #1 - second star
    There must be a better way to do this. Yuck.

--- src/1.cpp
+++ src/1.cpp
@@ -1,13 +1,27 @@
+#include <algorithm>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <unordered_set>
+#include <vector>
 #include <boost/algorithm/string/trim.hpp>
+
+using Position = std::pair<int, int>;
+std::string positionToString(const Position &p) {
+  return std::to_string(p.first) + "," + std::to_string(p.second);
+}
+
+int distance(const Position &p) {
+  return std::abs(p.first) + std::abs(p.second);
+}
 
 class Person {
   enum class Heading : int { North = 0, East, South, West };
 
 public:
-  Person() : heading_(Heading::North), position_(std::make_pair(0, 0)) {}
+  Person() : heading_(Heading::North), position_(std::make_pair(0, 0)) {
+    past_.push_back(position_);
+  }
   void turn(const std::string &movement) {
     if (movement[0] == 'L') {
       heading_ = static_cast<Heading>((static_cast<int>(heading_) - 1));
@@ -20,29 +34,49 @@ public:
   }
   void move(const std::string &movement) {
     if (heading() == Heading::North) {
-      position_.first += std::atoi(&movement[1]);
+      std::generate_n(std::back_inserter(past_), std::atoi(&movement[1]),
+                      [this]() {
+                        ++position_.first;
+                        return position_;
+                      });
     } else if (heading() == Heading::South) {
-      position_.first -= std::atoi(&movement[1]);
+      std::generate_n(std::back_inserter(past_), std::atoi(&movement[1]),
+                      [this]() {
+                        --position_.first;
+                        return position_;
+                      });
     } else if (heading() == Heading::East) {
-      position_.second += std::atoi(&movement[1]);
+      std::generate_n(std::back_inserter(past_), std::atoi(&movement[1]),
+                      [this]() {
+                        ++position_.second;
+                        return position_;
+                      });
     } else if (heading() == Heading::West) {
-      position_.second -= std::atoi(&movement[1]);
+      std::generate_n(std::back_inserter(past_), std::atoi(&movement[1]),
+                      [this]() {
+                        --position_.second;
+                        return position_;
+                      });
     }
-  }
-  int distance() const {
-    return std::abs(position_.first) + std::abs(position_.second);
-  }
-  std::string position() const {
-    return std::to_string(position_.first) + "," +
-           std::to_string(position_.second);
   }
   Heading heading() const {
     return static_cast<Heading>(static_cast<int>(heading_) % 4);
   }
+  const Position &position() const { return position_; }
+  Position firstDupPosition() {
+    std::unordered_set<std::string> positions;
+    auto dup =
+        std::find_if(past_.begin(), past_.end(), [&positions](const auto &p) {
+          auto result = positions.insert(positionToString(p));
+          return !result.second;
+        });
+    return *dup;
+  }
 
 private:
   Heading heading_;
-  std::pair<int, int> position_;
+  Position position_;
+  std::vector<Position> past_;
 };
 
 int main(int argc, char **argv) {
@@ -55,16 +89,19 @@ int main(int argc, char **argv) {
   std::ifstream in(argv[1]);
   Person p;
   std::cout << " --- Heading: " << static_cast<int>(p.heading())
-            << " --- Position: " << p.position() << "\n";
+            << " --- Position: " << positionToString(p.position()) << "\n";
   while (std::getline(in, movement, ',')) {
     boost::trim(movement);
     p.turn(movement);
     p.move(movement);
     std::cout << "Move: " << movement
               << " --- Heading: " << static_cast<int>(p.heading())
-              << " --- Position: " << p.position() << "\n";
+              << " --- Position: " << positionToString(p.position()) << "\n";
   }
 
-  std::cout << p.distance() << std::endl;
+  std::cout << "Location HQ: " << positionToString(p.firstDupPosition())
+            << "\n";
+  std::cout << "Distance to HQ: " << distance(p.firstDupPosition()) << "\n";
+  std::cout << "End: " << distance(p.position()) << "\n";
   return 0;
 }
