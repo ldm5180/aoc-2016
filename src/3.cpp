c209ebe41c9232440266cc7389e1ac8f56dc80a6
Lenny Maiorani <ldm5180@gmail.com>
2016-12-19 00:03:36 +0000

    AoC 2016 #3 - part 2

--- src/3.cpp
+++ src/3.cpp
@@ -6,12 +6,22 @@
 #include <boost/lexical_cast.hpp>
 
 int isTriangle(std::vector<int> t) {
-  if (t.empty()) {
-    return 0;
-  }
   std::sort(t.begin(), t.end());
   std::cout << "Triange: " << t[0] << "," << t[1] << "," << t[2] << "\n";
   return ((t[0] + t[1]) > t[2]) ? 1 : 0;
+}
+
+int numTriangles(const std::vector<int> &ts) {
+  if (ts.empty()) {
+    return 0;
+  }
+
+  int numTris = 0;
+  for (unsigned i = 0; i < 3; ++i) {
+    std::vector<int> aTri = {ts[0 + i], ts[3 + i], ts[6 + i]};
+    numTris += isTriangle(aTri);
+  }
+  return numTris;
 }
 
 int main(int argc, char **argv) {
@@ -20,18 +30,21 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  unsigned numTriangles = 0;
+  unsigned numTris = 0;
   std::string sizes;
   std::ifstream in(argv[1]);
+  std::vector<int> triangles;
   while (std::getline(in, sizes)) {
     int num;
-    std::vector<int> triangle;
     std::istringstream iss(sizes);
     while (iss >> num) {
-      triangle.push_back(num);
+      triangles.push_back(num);
     }
-    numTriangles += isTriangle(triangle);
+    if (triangles.size() == 9) {
+      numTris += numTriangles(triangles);
+      triangles.clear();
+    }
   }
-  std::cout << numTriangles << "\n";
+  std::cout << numTris << "\n";
   return 0;
 }
