db4e76a04e913e65a9cbb5994256835f6ba45412
Lenny Maiorani <ldm5180@gmail.com>
2016-12-25 07:58:19 +0000

    AoC 2016 #5 - part 2

--- src/5.cpp
+++ src/5.cpp
@@ -3,12 +3,15 @@
 #include <array>
 #include <algorithm>
 #include <range/v3/all.hpp>
+#include <boost/optional.hpp>
 
 using namespace ranges::v3;
 using Digest = std::array<unsigned char, 16>;
 
 bool isValidHash(const Digest &digest) {
-  return (digest[0] == 0) && (digest[1] == 0) && ((digest[2] & 0xF0) == 0);
+  auto loc = digest[2] & 0x0F;
+  return (digest[0] == 0) && (digest[1] == 0) && ((digest[2] & 0xF0) == 0) &&
+         (loc < 8);
 }
 
 Digest generateHash(const std::string &input, unsigned index) {
@@ -19,7 +22,22 @@ Digest generateHash(const std::string &input, unsigned index) {
   return digest;
 }
 
-int hashToChar(const Digest &d) { return d[2] & 0x0F; }
+std::tuple<int, int> hashToChar(const Digest &d) {
+  return std::make_tuple(d[2] & 0x0F, (d[3] & 0xF0) >> 4);
+}
+
+template <typename T, typename U> bool isFound(T &container, const U &val) {
+  if (container[std::get<0>(val)]) {
+    return true;
+  }
+  container[std::get<0>(val)] = std::get<1>(val);
+  return false;
+}
+
+template <typename T> bool isAllFound(const T &container) {
+  return !std::all_of(container.begin(), container.end(),
+                      [](auto &b) { return b; });
+}
 
 int main(int argc, char **argv) {
   if (argc <= 1) {
@@ -28,13 +46,17 @@ int main(int argc, char **argv) {
   }
 
   std::string input(argv[1]);
-  auto password =
+  std::vector<boost::optional<int>> positional(8);
+  std::vector<std::tuple<int, int>> password = yield_from(
       view::ints(0) |
-      view::transform([&input](int i) { return generateHash(input, i); }) |
+      view::transform([&input](auto i) { return generateHash(input, i); }) |
       view::remove_if([](const Digest &d) { return !isValidHash(d); }) |
       view::transform([](const Digest &d) { return hashToChar(d); }) |
-      view::take(8);
-  ranges::for_each(password, [](auto c) { std::cout << std::hex << c; });
+      view::remove_if(
+          [&positional](const auto &val) { return isFound(positional, val); }) |
+      view::take_while(
+          [&positional](const auto &) { return isAllFound(positional); }));
+  ranges::for_each(positional, [](auto &c) { std::cout << std::hex << *c; });
   std::cout << "\n";
   return 0;
 }
